691d77a51cda5f3953e1261e95936a4bbed7b48b
Jaska <jlaska@gmail.com>
2015-07-06 20:10:31 -0700

    Use avconv

--- utilities/transcode.py
+++ utilities/transcode.py
@@ -6,8 +6,9 @@ Could make this as configurable as desired.
 '''
 
 def shrink(in_file_path, out_file_path):
-  subprocess.call(['ffmpeg',
+  subprocess.call(['avconv',
                   '-i', in_file_path,
                   '-s', '854x480',
                   '-strict', 'experimental',
+                  '-c:a', 'copy',
                   out_file_path])
